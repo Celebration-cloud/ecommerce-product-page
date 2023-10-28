c3e01fe6ccfeb0f690b7e55f002ec3209653230d
Celebration-cloud <ojinguluc@gmail.com>
2023-10-28 21:36:57 +0100

    bug fix

---
+++ .history/src/App_20231028141334.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Navbar from './NavBar/Navbar';
+import TheBody from './The-Body/TheBody';
+import { useState } from 'react';
+
+function App() {
+  const [detail, setDetail] = useState([]);
+
+  const handleDetail = (info) => {
+    setDetail(prev => [...prev, info])
+    console.log(detail)
+  }
+  const handleRemove = (id) => {
+    setDetail(item => item.filter((item) => item.id !== id))
+  }
+  const handleTick = (id) => {
+    setDetail(item => item.map((item) => item.id === id && [{}...item, item.check:]))
+  }
+
+  return (
+    <div>
+    <Navbar detail={detail} handleRemove={handleRemove}/>
+    <TheBody handleDetail={handleDetail} />
+    </div>
+  );
+}
+
+export default App
+
+{/*  */}
---
+++ .history/src/The-Body/TheBody_20231028205602.jsx
@@ -0,0 +1,18 @@
+import ImageSlide from './ImageSlide';
+import ShoppingContent from './ShoppingContent';
+import './TheBody.css'
+
+function TheBody({ handleDetail }) {
+  return (
+    <div style={{ padding: "20px" }} className="container">
+      {shoppingContent.map((item) => (
+        <div key={item.id} className="content row">
+          <ImageSlide item={item} />
+          <ShoppingContent handleDetail={handleDetail} item={item} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TheBody
