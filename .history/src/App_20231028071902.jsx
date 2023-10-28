7e1506dfbb4aa67cad92ebf57b0a72f0d67c92f8
Celebration-cloud <ojinguluc@gmail.com>
2023-10-28 08:41:09 +0100

    remove option

---
+++ .history/src/App_20231028071902.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Navbar from './NavBar/Navbar';
+import TheBody from './The-Body/TheBody';
+import { useState } from 'react';
+
+function App() {
+  const [detail, setDetail] = useState([]);
+
+  const handleDetail = (info) => {
+    setDetail(prev => [...prev, info])
+    console.log(detail)
+  }
+
+  return (
+    <div>
+    <Navbar detail={detail}/>
+    <TheBody handleDetail={handleDetail} setDetail={set}/>
+    </div>
+  );
+}
+
+export default App
+
+{/*  */}
