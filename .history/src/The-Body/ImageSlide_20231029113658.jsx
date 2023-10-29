3bb8fc83355aa205ec2546271d17cc1794b5213c
Celebration-cloud <ojinguluc@gmail.com>
2023-10-29 14:38:25 +0100

    sticky bar and improvement in carousel

---
+++ .history/src/The-Body/ImageSlide_20231029113658.jsx
@@ -0,0 +1,88 @@
+import pic1 from '../../images/image-product-1.jpg'
+import pic2 from '../../images/image-product-2.jpg'
+import pic3 from '../../images/image-product-3.jpg'
+import pic4 from '../../images/image-product-4.jpg'
+function ImageSlide({item}) {
+    
+  return (
+    <div className="col">
+      {item..map((item) => {
+        
+      })}
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={item.imageUrls[0]} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={item.imageUrls[1]} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={item.imageUrls[2]} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={item.imageUrls[3]} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageSlide
---
+++ .history/src/shopping/ShoppingCart_20231029114152.jsx
@@ -0,0 +1,39 @@
+import { v4 as uuid } from "uuid";
+import pic1 from "../../images/image-product-1.jpg";
+import pic2 from "../../images/image-product-2.jpg";
+import pic3 from "../../images/image-product-3.jpg";
+import pic4 from "../../images/image-product-4.jpg";
+import pic1_1 from "../../images/pair-trainers (1).jpg";
+import pic1_3 from "../../images/pair-trainers (3).jpg";
+import pic1_4 from "../../images/pair-trainers (4).jpg";
+// import pic1_5 from "../../images/pair-trainers (5).jpg";
+// import pic1_6 from "../../images/pair-trainers (6).jpg";
+// import pic1_7 from "../../images/pair-trainers (7).jpg";
+// import pic1_8 from "../../images/pair-trainers (8).jpg";
+// import pic1_9 from "../../images/pair-trainers.jpg";
+const shoppingCart = [
+  {
+    id: uuid(),
+    company: "SNEAKER COMPANY",
+    productName: "Fall Limited Edition Sneakers",
+    description: `These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.`,
+    price: 250,
+    imageUrls: [pic1, pic2, pic3, pic4],
+    image: pic1,
+    discount: 50,
+  },
+  {
+    id: uuid(),
+    company: "Racool_studio",
+    productName: "A Pair Of Sneakers",
+    description: `Create great designs, faster
+Turn ideas into outstanding designs with high-quality vectors, photos, videos, mockups, and more`,
+    price: 500,
+    imageUrls: [pic1_1, pic1_2, pic1_3, pic1_4],
+    image: pic1_2,
+    discount: 50,
+  },
+];
+export default shoppingCart;
